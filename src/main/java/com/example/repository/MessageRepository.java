38580defdc6e360da9ef5d10a6d181847e0ec632
Tanya Costello <tanyahoo@gmail.com>
2025-05-10 00:58:06 +0000

    Initial commit

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,19 @@
 package com.example.repository;
+import org.springframework.data.jpa.repository.*;
 
-public interface MessageRepository {
+import com.example.entity.Message;
+
+// It also doesn't need to be marked as @Component or @Repository as this is inherited from the JPARepository
+public interface MessageRepository extends JpaRepository<Message, Integer>{
+
+// to do: write bespoke crud methods maybe using @Query?
+
+
+
+
+
+
+
 }
+
+
